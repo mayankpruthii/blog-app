0dfb1761bc0d3334a840109b9250ab1666e1679e
mayank <mayankpruthi123@gmail.com>
2022-06-12 14:10:27 +0530

    added validation for user auth

--- backend/src/routes/auth.ts
+++ backend/src/routes/auth.ts
@@ -2,7 +2,9 @@ import express from "express";
 const router = express.Router();
 
 import { signup } from "../controllers/auth.js";
+import { runValidation } from "../validators/index.js";
+import { userSignupValidator } from "../validators/auth.js";
 
-router.post("/signup", signup);
+router.post("/signup", userSignupValidator, runValidation, signup);
 
 export default router;
--- backend/src/validators/index.ts
+++ backend/src/validators/index.ts
@@ -1,11 +1,17 @@
-import {Request, Response, NextFunction} from "express";
+import { Request, Response, NextFunction } from "express";
 import { validationResult } from "express-validator";
 
-export const runValidation = (req: Request, res: Response, next: NextFunction) => {
-    const errors = validationResult(req);
-    if(!errors.isEmpty()) {
-        return res.status(422).json({
-            error: errors.array()[0].msg
-        })
-    }
-}
+export const runValidation = (
+	req: Request,
+	res: Response,
+	next: NextFunction,
+) => {
+	const errors = validationResult(req);
+	if (!errors.isEmpty()) {
+		return res.status(422).json({
+			ok: false, 
+			error: errors.array()[0].msg,
+		});
+	}
+    next();
+};
