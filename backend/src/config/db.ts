f4347d6cb3ed06ea8f8e1bbf49cb42d700d25089
mayank <mayankpruthi123@gmail.com>
2022-06-18 13:16:35 +0530

    switched mongourl from cloud to local, added .env.dev for reference

--- backend/src/config/db.ts
+++ backend/src/config/db.ts
@@ -4,18 +4,23 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const connectDatabase = async () => {
-	if (process.env.MONGO_URI) {
+	let mongo_url: string = "";
+	if ((process.env.ENV as string) === "production") {
+		mongo_url = process.env.MONGO_URI as string;
+	} else {
+		mongo_url = process.env.MONGO_LOCAL as string;
+	}
+	if (mongo_url) {
 		try {
-			const connection = await mongoose.connect(
-				process.env.MONGO_URI as string,
-			);
+			const connection = await mongoose.connect(mongo_url as string);
 			console.log(`MongoDB connected on ${connection.connection.host}`);
+			return;
 		} catch (error) {
 			let e = error as Error;
 			console.error("Error: ", e.message);
-			process.exit(1);
 		}
 	}
+	process.exit(1);
 };
 
 export default connectDatabase;
